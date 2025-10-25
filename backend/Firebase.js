4910639deace291673629abb80ef6a3a58b8410b
Abdelrahman Rady <abdelrahmanhrady@gmail.com>
2025-10-25 13:55:38 -0400

    Firebase setup

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -4,29 +4,20 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "", 
-//   measurementId: ""
-// } : {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "com",
-//   messagingSenderId: "",
-//   appId: "",
-//   measurementId: ""
-// }
+const firebaseConfig = {
+  apiKey: "AIzaSyCUj2iyW25K8QrTTMQf60dhvrKUHUKqTzU",
+  authDomain: "hackpsu-63bc0.firebaseapp.com",
+  projectId: "hackpsu-63bc0",
+  storageBucket: "hackpsu-63bc0.firebasestorage.app",
+  messagingSenderId: "86714705883",
+  appId: "1:86714705883:web:fe7e79e0d16e7a37c529fe",
+  measurementId: "G-PX5CMM7ND9"
+};
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // export const auth = getAuth(app);
-// export const storage = getStorage(app);
 // export const database = getFirestore(app);
-// export const analytics = () => getAnalytics(app);
+export const analytics = () => getAnalytics(app);
 
-// export default app
+export default app
